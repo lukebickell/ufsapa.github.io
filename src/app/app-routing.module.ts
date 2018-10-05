0cb0cc10004d60223500265784d101c239482acf
Luke Bickell <luke@bickell.com>
2018-10-05 10:31:34 -0400

    added homepage routing to member list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
+  { path: '', component: MemberListComponent },
   { path: 'members', component: MemberListComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent }
