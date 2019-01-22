64d912321fac7be1c2b368befa864b80314a38dc
Luke Bickell <luke@bickell.com>
2019-01-21 23:32:12 -0500

    refactored list and detail into member card component

--- src/app/member-card/member-card.component.ts
+++ src/app/member-card/member-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Member } from '../member';
 
 @Component({
   selector: 'app-member-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-card.component.css']
 })
 export class MemberCardComponent implements OnInit {
+
+  @Input() member: Member;
 
   constructor() { }
 
